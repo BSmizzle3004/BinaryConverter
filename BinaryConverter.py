995abdbbf9f21af6021fdf2643c782357504472a
BSmizzle3004 <231951@kedst.ac.uk>
2023-09-14 16:42:04 +0100

    Add decimal to binary logic

--- BinaryConverter.py
+++ BinaryConverter.py
@@ -2,13 +2,21 @@
 
 def decimal_to_binary(decimal):
     binary = ""
-    if decimal == 0:
-        return "0"
-    while decimal > 0:
-        remainder = decimal % 2
-        binary = str(remainder) + binary
-        decimal = decimal // 2
+    binary_list = [128,64,32,16,8,4,2,1]
+    
+    for i in binary_list:
+        if decimal >= i:
+            decimal = decimal - i
+            binary = binary + "1"
+        else:
+            binary = binary + "0"
+        
     return binary
+
+        
+
+
+    
 
 try:
     decimal = int(input("Enter a decimal number between 0 and 255: "))
@@ -19,3 +27,4 @@ try:
         print("Please enter a valid decimal number between 0 and 255.")
 except ValueError:
     print("Invalid input. Please enter a valid decimal number.")
+
